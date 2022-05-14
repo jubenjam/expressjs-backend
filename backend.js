c0a6a65cf6387bc6e15ee7eb8bd87102dcb5b138
jubenjam <49247806+jubenjam@users.noreply.github.com>
2022-05-14 13:48:07 -0700

    init

---
+++ backend.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const cors = require("cors");
+
+const taskServices = require("./models/task-services");
+const userServices = require("./models/user-services");
+
+const app = express();
+const port = 5005;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get("/tasks", async (req, res) => {
+  const category = req.query["category"];
+  const date = req.query["date"];
+  try {
+    const result = await taskServices.getTasks(category, date);
+    res.send({ task_list: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error ocurred in the server.");
+  }
+});
+
+app.get("/tasks/:id", async (req, res) => {
+  const id = req.params["id"];
+  const result = await taskServices.findTaskById(id);
+  if (result === undefined || result === null)
+    res.status(404).send("Resource not found.");
+  else {
+    res.send({ task_list: result });
+  }
+});
+
+app.get("/categories", async (req, res) => {
+  try {
+    const result = await taskServices.getCategories();
+    res.send({ category_list: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error ocurred in the server.");
+  }
+});
+
+app.post("/tasks", async (req, res) => {
+  const task = req.body;
+  const savedTask = await taskServices.addTask(task);
+  if (savedTask) res.status(201).send(savedTask);
+  else res.status(500).end();
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  const id = req.params["id"];
+  const result = await taskServices.deleteTaskById(id);
+  if (result === undefined || result === null)
+    res.status(404).send("Resource not found.");
+  else {
+    res.status(204).end();
+  }
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  const id = req.params["id"];
+  const result = await taskServices.editTask(req.body, id);
+  if (result === undefined || result === null)
+    res.status(500).send("Resource not found");
+  else {
+    res.status(201).end();
+  }
+});
+
+app.patch("/users/:id", async (req, res) => {
+  const id = req.params["id"];
+  const result = await userServices.editUser(req.body, id);
+  if (result === undefined || result === null)
+    res.status(500).send("Resource not found");
+  else {
+    res.status(201).end();
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const id = req.params["id"];
+  const result = await userServices.deleteUserById(id);
+  if (result === undefined || result === null)
+    res.status(404).send("Resource not found.");
+  else {
+    res.status(204).end();
+  }
+});
+
+app.get("/users", async (req, res) => {
+  const name = req.query["name"];
+  try {
+    const result = await userServices.getUsers(name);
+    res.send({ task_list: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error ocurred in the server.");
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  const id = req.params["id"];
+  const result = await userServices.findUserById(id);
+  if (result === undefined || result === null)
+    res.status(404).send("Resource not found.");
+  else {
+    res.send({ user_list: result });
+  }
+});
+
+app.post("/users", async (req, res) => {
+  const user = req.body;
+  const savedUser = await userServices.addUser(user);
+  if (savedUser) res.status(201).send(savedUser);
+  else res.status(500).end();
+});
---
+++ models/task-services.js
@@ -0,0 +1,141 @@
+const mongoose = require("mongoose");
+// const taskModel = require("./task");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// mongoose.set("debug", true);
+
+var conn = mongoose.createConnection(
+  "mongodb+srv://" +
+    process.env.MONGO_USER +
+    ":" +
+    process.env.MONGO_PWD +
+    "@" +
+    process.env.MONGO_CLUSTER +
+    "/" +
+    process.env.MONGO_DB +
+    "?retryWrites=true&w=majority",
+  // "mongodb://localhost:27017/users",
+  {
+    useNewUrlParser: true, //useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
+// .catch((error) => console.log(error));
+
+async function getTasks(category, date) {
+  let result;
+  if (category === undefined && date === undefined) {
+    result = await taskModel.find();
+  } else if (category && !date) {
+    result = await findTaskByCategory(category);
+  } else if (date && !category) {
+    result = await findTaskByDate(date);
+  } else {
+    result = await findTaskByCategoryAndDate(category, date);
+  }
+  return result;
+}
+
+async function getCategories() {
+  return await taskModel.distinct("category");
+}
+
+async function findTaskById(id) {
+  try {
+    return await taskModel.findById(id);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+}
+
+async function deleteTaskById(id) {
+  try {
+    return await taskModel.findByIdAndDelete(id);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+}
+
+async function addTask(task) {
+  try {
+    const taskToAdd = new taskModel(task);
+    const savedTask = await taskToAdd.save();
+    return savedTask;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+async function editTask(task, id) {
+  try {
+    return await taskModel.findByIdAndUpdate({ _id: id }, { $set: task });
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+async function findTaskByCategory(category) {
+  return await taskModel.find({ category: category });
+}
+
+async function findTaskByDate(date) {
+  return await taskModel.find({ date: date });
+}
+
+async function findTaskByCategoryAndDate(category, date) {
+  return await taskModel.find({ category: category, date: date });
+}
+
+async function completeTask(id, completed) {
+  try {
+    return await taskModel.findByIdAndUpdate(
+      { _id: id },
+      { completed: completed }
+    );
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+const TaskSchema = new mongoose.Schema(
+  {
+    task: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    date: {
+      type: Date,
+      required: true,
+      trim: true
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    completed: {
+      type: Boolean,
+      required: true,
+      trim: true
+    }
+  },
+  { collection: "task_list" }
+);
+
+const taskModel = conn.model("Task", TaskSchema);
+
+exports.deleteTaskById = deleteTaskById;
+exports.getTasks = getTasks;
+exports.findTaskById = findTaskById;
+exports.addTask = addTask;
+exports.editTask = editTask;
+exports.getCategories = getCategories;
+exports.completeTask = completeTask;
